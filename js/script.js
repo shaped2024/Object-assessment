9f319c3a6296bfe7e4fdd8757b208d5226b73c53
unknown <shalewapedro@yahoo.com>
2024-11-01 18:45:53 +0100

    add my project

---
+++ js/script.js
@@ -0,0 +1,82 @@
+
+<script>
+const book{
+    title: "To kill aMocking bird",
+     author: "Harper Lee",
+    year: 1960,
+    genre: "Friction"
+
+    document.write("const book");
+</script>
+const book{
+    title: "To kill aMocking bird",
+     author: "Harper Lee",
+    year: 1960,
+    genre: "Friction"
+
+    document.write("book");
+    //const library{
+    //title: ["Tough times dont last"],["We are at it again"],["The gods are not to be blamed"],["campus gist"],
+    //author: ["John Doe"], ["Oyinye Nnamdi"], ["Adams Shawn"], ["Sydney Lois"],
+    //Year: [2014], [1960], [2022], [2018],
+    //genre: ["literature"], ["art"], ["science"], ["friction"],
+
+    //let title [0] = "Tough times dont last";
+    //let title [1] = "We are at it again";
+    //let title [2] = "The gods are not to be blamed";
+    //let title [3] = "campus gist";
+
+    //functionSearchBooks(title[2]) {
+      //  return title [2]
+    //}l
+
+      //  if( this.title() == '2' ){
+		//	document.write( "Tough times dont last" );
+			
+		//}else{
+		//	document.write( "No Books Found" );
+		//}
+
+        //let text= "No Books Found";
+        //let result = text.toLowerCase;
+
+        //const year = 2014, 1960, 2022, 2018
+        //Const result = 
+      //  year.filter(check year) {
+        //    return year < =2022;
+
+        //}
+
+
+
+ //   }
+    
+//document.write("const book");
+
+//2
+//};
+
+
+
+   
+
+
+
+    
+    
+    
+    
+    
+   
+
+
+
+
+
+   
+
+
+
+
+
+
